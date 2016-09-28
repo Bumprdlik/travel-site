10a45fca4d69010074ab86e458d5388a026ff558
Vaclav <vencator@seznam.cz>
2016-09-28 23:03:12 +0200

    Completed the reveal on scroll behavior.

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -58,20 +58,25 @@
 
 	var _jquery2 = _interopRequireDefault(_jquery);
 
+	var _StickyHeader = __webpack_require__(5);
+
+	var _StickyHeader2 = _interopRequireDefault(_StickyHeader);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var mobileMenu = new _MobileMenu2.default();
 	new _RevealOnScroll2.default((0, _jquery2.default)(".feature-item"), "85%");
 	new _RevealOnScroll2.default((0, _jquery2.default)(".testimonial"), "60%");
+	var stickyHeader = new _StickyHeader2.default();
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	Object.defineProperty(exports, "__esModule", {
-		value: true
+	  value: true
 	});
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -85,30 +90,30 @@
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	var MobileMenu = function () {
-		function MobileMenu() {
-			_classCallCheck(this, MobileMenu);
+	  function MobileMenu() {
+	    _classCallCheck(this, MobileMenu);
 
-			this.siteHeader = (0, _jquery2.default)('.site-header');
-			this.menuIcon = (0, _jquery2.default)(".site-header__menu-icon");
-			this.menuContent = (0, _jquery2.default)(".site-header__menu-content");
-			this.events();
-		}
+	    this.siteHeader = (0, _jquery2.default)(".site-header");
+	    this.menuIcon = (0, _jquery2.default)(".site-header__menu-icon");
+	    this.menuContent = (0, _jquery2.default)(".site-header__menu-content");
+	    this.events();
+	  }
 
-		_createClass(MobileMenu, [{
-			key: 'events',
-			value: function events() {
-				this.menuIcon.click(this.toggleTheMenu.bind(this));
-			}
-		}, {
-			key: 'toggleTheMenu',
-			value: function toggleTheMenu() {
-				this.menuContent.toggleClass("site-header__menu-content--is-visible");
-				this.siteHeader.toggleClass("site-header--is-expanded");
-				this.menuIcon.toggleClass("site-header__menu-icon--close-x");
-			}
-		}]);
+	  _createClass(MobileMenu, [{
+	    key: "events",
+	    value: function events() {
+	      this.menuIcon.click(this.toggleTheMenu.bind(this));
+	    }
+	  }, {
+	    key: "toggleTheMenu",
+	    value: function toggleTheMenu() {
+	      this.menuContent.toggleClass("site-header__menu-content--is-visible");
+	      this.siteHeader.toggleClass("site-header--is-expanded");
+	      this.menuIcon.toggleClass("site-header__menu-icon--close-x");
+	    }
+	  }]);
 
-		return MobileMenu;
+	  return MobileMenu;
 	}();
 
 	exports.default = MobileMenu;
@@ -10346,7 +10351,7 @@
 	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
-		value: true
+	  value: true
 	});
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -10364,38 +10369,38 @@
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	var RevealOnScroll = function () {
-		function RevealOnScroll(els, offset) {
-			_classCallCheck(this, RevealOnScroll);
+	  function RevealOnScroll(els, offset) {
+	    _classCallCheck(this, RevealOnScroll);
 
-			this.itemsToReveal = els;
-			this.offsetPercentage = offset;
-			this.hideInitially();
-			this.createWaypoints();
-		}
+	    this.itemsToReveal = els;
+	    this.offsetPercentage = offset;
+	    this.hideInitially();
+	    this.createWaypoints();
+	  }
 
-		_createClass(RevealOnScroll, [{
-			key: 'hideInitially',
-			value: function hideInitially() {
-				this.itemsToReveal.addClass("reveal-item");
-			}
-		}, {
-			key: 'createWaypoints',
-			value: function createWaypoints() {
-				var that = this;
-				this.itemsToReveal.each(function () {
-					var currentItem = this;
-					new Waypoint({
-						element: currentItem,
-						handler: function handler() {
-							(0, _jquery2.default)(currentItem).addClass("reveal-item--is-visible");
-						},
-						offset: that.offsetPercentage
-					});
-				});
-			}
-		}]);
+	  _createClass(RevealOnScroll, [{
+	    key: 'hideInitially',
+	    value: function hideInitially() {
+	      this.itemsToReveal.addClass("reveal-item");
+	    }
+	  }, {
+	    key: 'createWaypoints',
+	    value: function createWaypoints() {
+	      var that = this;
+	      this.itemsToReveal.each(function () {
+	        var currentItem = this;
+	        new Waypoint({
+	          element: currentItem,
+	          handler: function handler() {
+	            (0, _jquery2.default)(currentItem).addClass("reveal-item--is-visible");
+	          },
+	          offset: that.offsetPercentage
+	        });
+	      });
+	    }
+	  }]);
 
-		return RevealOnScroll;
+	  return RevealOnScroll;
 	}();
 
 	exports.default = RevealOnScroll;
@@ -11162,6 +11167,61 @@
 	  Waypoint.Adapter = NoFrameworkAdapter
 	}())
 	;
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _jquery = __webpack_require__(2);
+
+	var _jquery2 = _interopRequireDefault(_jquery);
+
+	var _noframework = __webpack_require__(4);
+
+	var _noframework2 = _interopRequireDefault(_noframework);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var StickyHeader = function () {
+	  function StickyHeader() {
+	    _classCallCheck(this, StickyHeader);
+
+	    this.siteHeader = (0, _jquery2.default)(".site-header");
+	    this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
+	    this.createHeaderWaypoint();
+	  }
+
+	  _createClass(StickyHeader, [{
+	    key: 'createHeaderWaypoint',
+	    value: function createHeaderWaypoint() {
+	      var that = this;
+	      new Waypoint({
+	        element: this.headerTriggerElement[0],
+	        handler: function handler(direction) {
+	          if (direction == "down") {
+	            that.siteHeader.addClass("site-header--dark");
+	          } else {
+	            that.siteHeader.removeClass("site-header--dark");
+	          }
+	        }
+	      });
+	    }
+	  }]);
+
+	  return StickyHeader;
+	}();
+
+	exports.default = StickyHeader;
 
 /***/ }
 /******/ ]);
--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,5 @@ require('./gulp/tasks/scripts');
 
 
 
+
+
